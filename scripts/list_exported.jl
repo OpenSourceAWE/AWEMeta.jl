7d1381b0ef66c7793011eb7f008aa1ca34134855
Uwe Fechner <fechner@aenarete.eu>
2025-07-12 15:21:13 +0200

    Find duplicates

--- scripts/list_exported.jl
+++ scripts/list_exported.jl
@@ -30,3 +30,25 @@ for mod in modules
     @eval using $(Symbol(mod))
     add_exported!(dict, mod)
 end
+
+function print_common_symbols(dict::Dict{String, Vector{Symbol}})
+    # Flatten all symbols from all vectors into one array, keeping track of which key they came from
+    symbol_to_keys = Dict{Symbol, Set{String}}()
+    for (k, syms) in dict
+        for s in syms
+            if haskey(symbol_to_keys, s)
+                push!(symbol_to_keys[s], k)
+            else
+                symbol_to_keys[s] = Set([k])
+            end
+        end
+    end
+    # Filter symbols that appear in more than one key
+    common_symbols = [s for (s, keys) in symbol_to_keys if length(keys) > 1]
+    println("Symbols present in more than one module:")
+    for s in common_symbols
+        println(s)
+    end
+end
+
+print_common_symbols(dict)
